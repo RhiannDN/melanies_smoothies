e9e2a4233862f22106b50aa24266cfc828a09d02
RhiannDN <R.DECARDI-NELSON@NHS.NET>
2026-06-29 16:50:32 +0100

    Comment out Streamlit dataframe display and stop
    Comment out dataframe display and stop functions.

--- stream_app.py
+++ stream_app.py
@@ -22,8 +22,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert the Snowpark dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
@@ -32,7 +32,6 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
